3bfe4e12ed9510ca85c8280c7e4a1d1bae0609a5
djmorel <djmorel@bu.edu>
2019-09-09 11:18:25 -0400

    Added graph to RoomDetails activity.

--- app/src/main/java/team33/ec463/com/ec463team33miniproject/RoomDetails.java
+++ app/src/main/java/team33/ec463/com/ec463team33miniproject/RoomDetails.java
@@ -1,13 +1,36 @@
 package team33.ec463.com.ec463team33miniproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.provider.ContactsContract;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class RoomDetails extends AppCompatActivity {
+public class RoomDetails extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    // Instantiate the layout types
+    LineGraphSeries<DataPoint> series;
     TextView targetRoomName;
     Button deleteRoom_button;
 
@@ -15,10 +38,51 @@ public class RoomDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        // Data values for the plot
+        double x = 1.0;
+        double y;
+        double[] temp = new double[]{349, 344, 339, 334, 331, 327, 325, 323, 321, 338, 340, 341, 346, 342, 344};
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i = 0; i < 15; i++)
+        {
+            x++;
+            y = temp[i] / 10.0;
+            series.appendData(new DataPoint(x,y), true, 100);
+        }
+        graph.addSeries(series);
+
+        // Custom label formatter to show am/pm and F
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // Show normal x values
+                    return super.formatLabel(value, isValueX) + ":00 am";
+                } else {
+                    // Show F for y values
+                    return super.formatLabel(value, isValueX) + " F";
+                }
+            }
+        });
 
         // Connect the layout elements to variables
         targetRoomName = (TextView) findViewById(R.id.targetRoomName_textview);
-        deleteRoom_button = (Button) findViewById(R.id.deleteRoom_button);
+        deleteRoom_button = (Button) findViewById(R.id.deleteRoom_Button);
 
         // Get Room Name
         Bundle roomBundle = getIntent().getExtras();
@@ -26,5 +90,67 @@ public class RoomDetails extends AppCompatActivity {
         {
             targetRoomName.setText(roomBundle.getString("RoomName"));
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.room_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_rooms)
+        {
+            // Go to the Rooms page
+            Intent roomsIntent = new Intent(getApplicationContext(), Rooms.class);
+            startActivity(roomsIntent);
+        }
+        else if (id == R.id.nav_devices)
+        {
+            // Go to the Devices page
+            Intent devicesIntent = new Intent(getApplicationContext(), Devices.class);
+            startActivity(devicesIntent);
+        }
+        else if (id == R.id.nav_signout)
+        {
+            // Go to Login page
+            Intent signoutIntent = new Intent(getApplicationContext(), Login.class);
+            startActivity(signoutIntent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
