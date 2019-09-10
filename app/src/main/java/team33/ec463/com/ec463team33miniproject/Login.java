cebfa16c526e531dfb5fd4e30b5fd6a760f092a1
thuchngu <thucnguyen61098@gmail.com>
2019-09-09 21:09:05 -0400

    Added device class and snapshot listener for new devices

--- app/src/main/java/team33/ec463/com/ec463team33miniproject/Login.java
+++ app/src/main/java/team33/ec463/com/ec463team33miniproject/Login.java
@@ -1,6 +1,7 @@
 package team33.ec463.com.ec463team33miniproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +9,18 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
-    private static final String TAG = "Rooms";
+    private static final String RTAG = "Rooms";
+    private static final String DTAG = "Devices";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,23 +42,25 @@ public class Login extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "Added new room with ID: " + documentReference.getId());
+                        Log.d(RTAG, "Added new room with ID: " + documentReference.getId());
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Could not add new room", e);
+                        Log.w(RTAG, "Could not add new room", e);
                     }
                 });
     }
 
-    public void addNewDevice(String name, String type, int idVal, String room){
+    public void addNewDevice(final Device newDev, String room){
         //create new device with a name, type, and ID
         Map<String, Object> device = new HashMap<>();
-        device.put("Name", name);
-        device.put("Type", type);
-        device.put("ID", idVal);
+        device.put("Name", newDev.name);
+        device.put("Type", newDev.type);
+        device.put("ID", newDev.idVal);
+        device.put("data", newDev.data);
+        device.put("device", newDev);
 
         // Add new device to subcollection of specified room with a new ID
         datab.collection("rooms").document(room).collection("devices")
@@ -61,49 +68,69 @@ public class Login extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "Added new device with ID: " + documentReference.getId());
+                        Log.d(DTAG, "Added new device with ID: " + documentReference.getId());
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Could not add new device", e);
+                        Log.w(DTAG, "Could not add new device", e);
                     }
                 });
+        //add a snapshot listener whenever a new device is added to listen for new data
+        final DocumentReference deviceRef = datab.collection("rooms").document(room).collection("devices").document(newDev.name);
+        deviceRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot deviceSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(RTAG, "Failed to listen", e);
+                    return;
+                }
+                if(deviceSnapshot != null && deviceSnapshot.exists() && (newDev.type.equals("Thermometer"))){
+                    Log.d(DTAG, "Current temperature: " + deviceSnapshot.get("data"));
+                }else if(deviceSnapshot != null && deviceSnapshot.exists() && (newDev.type.equals("Hygrometer"))){
+                    Log.d(DTAG, "Current humidity: " + deviceSnapshot.get("data"));
+                }else{
+                    Log.d(DTAG, "Null");
+                }
+            }
+        });
     }
 
     public void deleteDevice(String name, String room) {
         //delete device from specified room subcollection
-        datab.collection("rooms").document(room).collection("devices").document(name)
+        final DocumentReference deviceRef = datab.collection("rooms").document(room).collection("devices").document(name);
+        deviceRef
                 .delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "Device was deleted");
+                        Log.d(DTAG, "Device was deleted");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "Device was not deleted");
+                        Log.d(DTAG, "Device was not deleted");
                     }
                 });
     }
 
     public void deleteRoom(String room) {
         //delete room from collection
-        datab.collection("rooms").document(room)
+        final DocumentReference roomRef =  datab.collection("rooms").document(room);
+        roomRef
                 .delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "Room was deleted");
+                        Log.d(RTAG, "Room was deleted");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "Room was not deleted");
+                        Log.d(RTAG, "Room was not deleted");
                     }
                 });
     }
